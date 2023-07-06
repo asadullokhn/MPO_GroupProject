543aaacd7da3d2c1602f338c9abd0e30650ca42d
Asadullokh Nurullaev <asadullonurullaev@gmail.com>
2023-07-06 18:33:07 +0800

    Pretty MPO letters

--- MPO.cpp
+++ MPO.cpp
@@ -5,7 +5,21 @@ int main()
 {
     system("cls");
 
-    cout << "The Malaysian Philharmonic Orchestra (MPO)\n\n";
+    string image[] = {
+        "       <<<    MMM     MMM       PPPPPPPPPPP        OOOOOOOOOO     >>>",
+        "     <<<      M  M   M  M       PP        PP     OO          OO     >>>",
+        "   <<<        M   M M   M       PP        PP    OO             OO      >>>",
+        " <<<          M    M    M       PPPPPPPPPPP    OO               OO       >>>",
+        "   <<<        M         M       PP              OO             OO      >>>",
+        "     <<<      M         M       PP               OO           OO    >>>",
+        "       <<<    M         M       PP                 OOOOOOOOOO     >>>"};
+
+    cout << "\033[1;31m";
+    for (int i = 0; i < 7; i++)
+        cout << image[i] << endl;
+    cout << "\033[0m\n";
+
+    cout << "The Malaysian Philharmonic Orchestra\n\n";
     cout << "[\033[1;31m1\033[0m] \033[1;32mUser\033[0m\n";
     cout << "[\033[1;31m2\033[0m] \033[1;32mAdmin\033[0m\n\n";
     cout << "[\033[1;31m0\033[0m] \033[1;32mExit\033[0m";
